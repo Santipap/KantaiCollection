f7e28d5856a8ebd362a3931ac9e1efb0ba86fc7d
Santipap <hibiki307@gmail.com>
2016-04-05 11:06:28 +0700

    App Guide 2# up to save

--- app/src/main/java/netkasakice_pbru/santipap/guideapp/MainActivity.java
+++ app/src/main/java/netkasakice_pbru/santipap/guideapp/MainActivity.java
@@ -1,13 +1,41 @@
 package netkasakice_pbru.santipap.guideapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindWidget(); //)ประกาศตัวแปรที่เราจะใช้
+        buttoncontroler();
+
+    }//main method
+
+   // ในส่วนของ () ที่ 2 จะเป็นกสรกำหนดต่าต่างเกีย่วกับการรแสดงผมทั้งหมด
+    private void buttoncontroler() //ส่วนที่ใช้แสดงผล อันนี้สำหรับ link ไปยัง www
+    {
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.facebook.com/profile.php?id=100009860237292"));
+                startActivity(intent);
+            }
+        });
     }
+
+    private void bindWidget(){ //ส่วนที่ใช้กำหนดค่าตัวแปร กำหนดชื่อตัวแปรที่สร้างใหม่ใน text = id ของ object ที่เราสร้างไว้
+        about=(Button) findViewById(R.id.button);
+
+    }//bindWidget
+
+
 }
---
+++ app/src/main/java/netkasakice_pbru/santipap/guideapp/MyAdapter.java
@@ -0,0 +1,68 @@
+package netkasakice_pbru.santipap.guideapp;
+
+import android.content.Context;
+import android.graphics.drawable.LayerDrawable;
+import android.media.Image;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by hibiki on 4/5/2016.
+ */
+public class MyAdapter extends BaseAdapter{
+    //Explicit ประกาศตัวแปร
+    private Context context;
+    private int[] iconInts;
+    private String[] titleString,detailStrings;
+
+    public MyAdapter(Context context, int[] iconInts, String[] titleString, String[] datailStrings) { // ait+insert
+        this.context = context;
+        this.iconInts = iconInts;
+        this.titleString = titleString;
+        this.detailStrings = datailStrings;
+    }//ส่วนประกาศตัวแปร
+
+
+    @Override
+    public int getCount() {
+        return iconInts.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view1 = layoutInflater.inflate(R.layout.traffic_listview,viewGroup,false);
+
+//for icon
+        ImageView iconImageView = (ImageView) view1.findViewById(R.id.imageView);
+        iconImageView.setImageResource(iconInts[i]);
+
+        //fot title
+        TextView titleTextView = (TextView) view1.findViewById(R.id.textView2);
+        titleTextView.setText(titleString[i]);
+
+        //for detail
+        TextView detailTextView = (TextView) view1.findViewById(R.id.textView3);
+        detailTextView.setText(detailStrings[i]);
+
+        return view1;
+    }
+}// MyAdapter class ที่สร้างใหม่
+
